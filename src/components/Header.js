4af3dfc79b414a22cf1a9e9f4947cd3ce16b0f4c
Humza Butt <Humzab1711@hotmail.com>
2025-07-16 22:39:17 +0100

    Add new icons and update Header component with logo image

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,7 @@ const Header = () => {
       <div className="header-container">
         <div className="header-left">
           <div className="header-logo">
-            {/* Placeholder SVG logo */}
-            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="16" cy="16" r="16" fill="#176D6A" />
-              <path d="M16 8v8l6 3" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-              <circle cx="16" cy="16" r="4" fill="#fff" />
-            </svg>
+            <img src="/LifeSmart.png" alt="LifeSmart Logo" />
           </div>
           <span className="header-lifesmart">LifeSmart</span>
         </div>
